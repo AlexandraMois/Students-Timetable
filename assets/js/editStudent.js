6f414f4603fd78feea9e7ad86f1b91136061d05f
Alexandra Mois <mois.alexandra7@gmail.com>
2017-07-26 10:43:19 +0300

    change the path

--- assets/js/editStudent.js
+++ assets/js/editStudent.js
@@ -4,7 +4,7 @@ function saveStudent(studentId) {
 
 	modal.find('.errors').html('').removeClass('eroare-edit');
 
-	$.post( "/students/update", 
+	$.post( "/students", 
 		$("#form-update-student-" + studentId).serialize() 
 	)
 	  .done(function( data ) {
