930d62a8fb79e2013c5c52dba97dbe780276f6b8
Alexandra Mois <mois.alexandra7@gmail.com>
2017-07-26 10:28:55 +0300

    change js

--- assets/js/editStudent.js
+++ assets/js/editStudent.js
@@ -10,8 +10,9 @@ function saveStudent(studentId) {
 	  .done(function( data ) {
 	  	response = JSON.parse(data);
 	  	if (response.status == 'success') {
-	  		modal.modal("hide");
-	  		// $('.modal-backdrop.in').hide();
+	  		modal..on('hidden', function () {
+		        $(this).remove();
+		    });
 			$('#status-update-student').removeClass('hidden').addClass('reusit').html('Studentul a fost salvat cu succes!');
 			
 	  	} else {
